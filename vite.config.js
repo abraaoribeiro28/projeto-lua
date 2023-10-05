23fd2a90f064b700ec2a521d0fab5ca3e0057d67
abraao <abraao@pbsoft.com.br>
2023-10-04 21:59:30 -0300

    feat: adicionando arquivo tailwindo css com recusos necessarios para o breeze

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/css/home.css',
+                'resources/css/tailwind.css',
                 'resources/js/app.js',
             ],
             refresh: true,
